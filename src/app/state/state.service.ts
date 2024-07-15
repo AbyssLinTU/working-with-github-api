d1b4a4b72712c82c5ba49fd58fe84e0100858d49
AbyssLinTU <marvovolen@gmail.com>
2024-07-15 11:34:36 +0300

    follow-unfollow mechanic

--- src/app/state/state.service.ts
+++ src/app/state/state.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-interface IState {}
+const LS_FAV_KEY = 'LS_FAV_KEY';
+interface IState {
+  favorites: string[];
+}
 
 @Injectable({ providedIn: 'root' })
 export class StateService {
-  private _state: IState = {};
+  private _state: IState = {
+    favorites: JSON.parse(localStorage.getItem(LS_FAV_KEY) ?? '[]'),
+  };
 
   getState() {
     return this._state;
@@ -11,5 +16,15 @@ export class StateService {
 
   setState(data: IState) {
     this._state = data;
+  }
+  addFavorites(id: number) {
+    this._state.favorites.push(id.toString());
+    localStorage.setItem(LS_FAV_KEY, JSON.stringify(this._state.favorites));
+  }
+  removeFavorites(id: number) {
+    this._state.favorites = this._state.favorites.filter(
+      (f) => f != id.toString()
+    );
+    localStorage.setItem(LS_FAV_KEY, JSON.stringify(this._state.favorites));
   }
 }
